4eeb324df7fe906fb8934179ba419344eafbbe1d
Shao Jin <sj3192@nyu.edu>
2025-05-04 18:30:14 -0500

    test_getCardCounts_oneOfEachType

--- src/test/java/explodingkittens/model/DeckTest.java
+++ src/test/java/explodingkittens/model/DeckTest.java
@@ -69,5 +69,31 @@ public class DeckTest {
         assertEquals(1, cardCounts.get("SkipCard"));
     }
 
-    
+    @Test
+    public void test_getCardCounts_oneOfEachType() {
+        Deck deck = new Deck();
+        deck.addCard(new SkipCard());
+        deck.addCard(new ShuffleCard());
+        deck.addCard(new AttackCard());
+        deck.addCard(new DefuseCard());
+        deck.addCard(new SeeTheFutureCard());
+        deck.addCard(new NopeCard());
+        deck.addCard(new CatCard(CatType.TACOCAT));
+        deck.addCard(new CatCard(CatType.BEARD_CAT));
+        deck.addCard(new CatCard(CatType.CATTERMELON));
+        deck.addCard(new CatCard(CatType.RAINBOW_CAT));
+        
+        Map<String, Integer> cardCounts = deck.getCardCounts();
+        assertEquals(10, cardCounts.size());
+        assertEquals(1, cardCounts.get("SkipCard"));
+        assertEquals(1, cardCounts.get("ShuffleCard"));
+        assertEquals(1, cardCounts.get("AttackCard"));
+        assertEquals(1, cardCounts.get("DefuseCard"));
+        assertEquals(1, cardCounts.get("SeeTheFutureCard"));
+        assertEquals(1, cardCounts.get("NopeCard"));
+        assertEquals(1, cardCounts.get("CatCard_TACOCAT"));
+        assertEquals(1, cardCounts.get("CatCard_BEARD_CAT"));
+        assertEquals(1, cardCounts.get("CatCard_CATTERMELON"));
+        assertEquals(1, cardCounts.get("CatCard_RAINBOW_CAT"));
+    }
 }
