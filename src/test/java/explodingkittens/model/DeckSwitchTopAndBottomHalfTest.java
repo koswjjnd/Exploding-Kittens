171e0e8803cae82f67ff02319587ea9d12bb212e
Shao Jin <sj3192@nyu.edu>
2025-05-25 16:17:31 -0500

    finish deckswitch test case 5

--- src/test/java/explodingkittens/model/DeckSwitchTopAndBottomHalfTest.java
+++ src/test/java/explodingkittens/model/DeckSwitchTopAndBottomHalfTest.java
@@ -63,5 +63,28 @@ public class DeckSwitchTopAndBottomHalfTest {
         assertEquals(List.of(card3, card4, card1, card2), cards, "Even size deck should swap top/bottom half");
     }
 
-    
+    @Test
+    public void testSwitchTopAndBottomHalf_oddSizeDeck() {
+        // Deck: [A, B, C, D, E]
+        Card card1 = new AttackCard();
+        Card card2 = new SkipCard();
+        Card card3 = new DefuseCard();
+        Card card4 = new ShuffleCard();
+        Card card5 = new SeeTheFutureCard();
+        deck.addCard(card1);
+        deck.addCard(card2);
+        deck.addCard(card3);
+        deck.addCard(card4);
+        deck.addCard(card5);
+
+        deck.switchTopAndBottomHalf();
+
+        // Expect: [D, E, C, A, B]
+        List<Card> cards = deck.getCards();
+        assertEquals(card4, cards.get(0));
+        assertEquals(card5, cards.get(1));
+        assertEquals(card3, cards.get(2), "Middle card stays in place");
+        assertEquals(card1, cards.get(3));
+        assertEquals(card2, cards.get(4));
+    }
 }
