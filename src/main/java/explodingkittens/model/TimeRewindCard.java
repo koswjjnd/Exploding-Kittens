4323c59455499ee0a47a6e82d4900429b6ac758d
qimengchao981 <u1297640@umail.utah.edu>
2025-05-26 00:40:32 -0500

    create BVA document for Time rewind card

---
+++ src/main/java/explodingkittens/model/TimeRewindCard.java
@@ -0,0 +1,13 @@
+package explodingkittens.model;
+
+import java.util.List;
+
+/**
+ * Represents a Time Rewind card in the game.
+ * When played, moves the top 3 cards from the deck to the bottom,
+ * effectively avoiding potentially dangerous cards temporarily.
+ */
+public class TimeRewindCard{
+
+  
+}
