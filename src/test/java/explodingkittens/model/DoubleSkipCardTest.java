e1a6c8ff83d0de52abc4b92ed1e4ecd4b64552e8
Liming Luo <limingluo2025@u.northwestern.edu>
2025-05-30 10:19:01 -0500

    DoubleSkipCardmaxLT

--- src/test/java/explodingkittens/model/DoubleSkipCardTest.java
+++ src/test/java/explodingkittens/model/DoubleSkipCardTest.java
@@ -103,5 +103,20 @@ class DoubleSkipCardTest {
         assertEquals(player1, turnOrder.get(0));
         verify(player1).setLeftTurns(0);
     }
+
+    @Test
+    void testEffectWithMaxValueLeftTurns() {
+        // Test Case 6: leftTurn = Integer.MAX_VALUE
+        turnOrder.add(player1);
+        turnOrder.add(player2);
+        when(player1.getLeftTurns()).thenReturn(Integer.MAX_VALUE);
+        
+        doubleSkipCard.effect(turnOrder, gameDeck);
+        
+        assertEquals(2, turnOrder.size());
+        assertEquals(player1, turnOrder.get(0));
+        assertEquals(player2, turnOrder.get(1));
+        verify(player1).setLeftTurns(Integer.MAX_VALUE - 2);
+    }
     
 } 
